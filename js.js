b5c5eb84ade9a86da94ef8c86e8da12a3865ddc9
Saoa35 <saoamah@gmail.com>
2022-02-14 20:43:29 +0100

    rebuild getWeather func

--- js.js
+++ js.js
@@ -4,17 +4,17 @@ let urlKyiv = 'http://api.openweathermap.org/data/2.5/weather?id=703448&appid=bf
 let urlLondon = 'http://api.openweathermap.org/data/2.5/weather?id=2643743&appid=bf35cac91880cb98375230fb443a116f';
 let urlNewYork = 'http://api.openweathermap.org/data/2.5/weather?id=5128638&appid=bf35cac91880cb98375230fb443a116f';
 
-function getWearher (url) {
+function getWearher (url, div) {
     fetch(url) 
                 .then(resp => resp.json())
                 .then(json => {
-                    output.textContent = '';
-                    output.textContent += '  ' + json.name;
-                    output.textContent += '  ' + json.weather[0].description;
-                    output.textContent += '  ' + Math.trunc((json.main.temp - 32) / 1.8) + '℃';
+                    div.textContent = '';
+                    div.textContent += '  ' + json.name;
+                    div.textContent += '  ' + json.weather[0].description;
+                    div.textContent += '  ' + Math.trunc((json.main.temp - 32) / 1.8) + ' ℃';
                     const img = document.createElement('img');
                     img.src = 'http://openweathermap.org/img/wn/' + json.weather[0]['icon'] + '@2x.png';
-                    output.append(img);
+                    div.append(img);
                 })
                 // .then(item => console.log(item))
                 .catch(error => console.log(error.message));
@@ -22,4 +22,4 @@ function getWearher (url) {
 
 // getWearher(urlKyiv);
 // getWearher(urlLondon);
-getWearher(urlNewYork);
+getWearher(urlNewYork, output);
