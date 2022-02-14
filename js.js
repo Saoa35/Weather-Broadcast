aa5f0581cc415a5e1a622ff03619b142863b1ac3
Saoa35 <saoamah@gmail.com>
2022-02-14 20:05:41 +0100

    added temperature

--- js.js
+++ js.js
@@ -4,17 +4,20 @@ let urlKyiv = 'http://api.openweathermap.org/data/2.5/weather?id=703448&appid=bf
 let urlLondon = 'http://api.openweathermap.org/data/2.5/weather?id=2643743&appid=bf35cac91880cb98375230fb443a116f';
 let urlNewYork = 'http://api.openweathermap.org/data/2.5/weather?id=5128638&appid=bf35cac91880cb98375230fb443a116f';
 
-function getData (url) {
+function getWearher (url) {
     fetch(url) 
                 .then(resp => resp.json())
                 .then(json => {
                     output.textContent = '';
                     output.textContent += '  ' + json.name;
                     output.textContent += '  ' + json.weather[0].description;
-                    
+                    output.textContent += '  ' + Math.trunc((json.main.temp - 32) / 1.8);
+
                 })
                 // .then(item => console.log(item))
                 .catch(error => console.log(error.message));
 }
 
-getData(urlKyiv);
+getWearher(urlKyiv);
+// getWearher(urlLondon);
+// getWearher(urlNewYork);
