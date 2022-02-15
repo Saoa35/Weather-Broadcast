377ef93ca69b7eaa0f7b0e226e55625063e7e3eb
Saoa35 <saoamah@gmail.com>
2022-02-15 23:24:31 +0100

    recoding getWeather func

--- js.js
+++ js.js
@@ -3,24 +3,27 @@ let urlKyiv = 'http://api.openweathermap.org/data/2.5/weather?id=703448&appid=bf
 let urlLondon = 'http://api.openweathermap.org/data/2.5/weather?id=2643743&appid=bf35cac91880cb98375230fb443a116f';
 let urlNewYork = 'http://api.openweathermap.org/data/2.5/weather?id=5128638&appid=bf35cac91880cb98375230fb443a116f';
 
-function getWearher (url, div) {
-    fetch(url) 
+
+    function getWearher (url, div) {
+        fetch(url) 
                 .then(resp => resp.json())
                 .then(json => {
-                    // div.textContent = '';
-                    const p = document.createElement('p');
-                    p.innerText += '  ' + json.name;
-                    p.innerText += '  ' + json.weather[0].description;
-                    p.innerText += '  ' + Math.round(json.main.temp - 273.15) + ' ℃';
-                    const img = document.createElement('img');
-                    img.src = 'http://openweathermap.org/img/wn/' + json.weather[0]['icon'] + '@2x.png';
-                    div.append(p);
-                    div.append(img);
+                    const cityName = json.name,
+                          weatherDescr = json.weather[0].description,
+                          tempCelsius = Math.round(json.main.temp - 273.15) + ' ℃',
+                          img = document.createElement('img');
+                          img.src = 'http://openweathermap.org/img/wn/' + json.weather[0]['icon'] + '@2x.png';
+
+                    [cityName, tempCelsius, weatherDescr, img].map(el => {
+                        let li = document.createElement('li')
+                        li.append(el);
+                        div.append(li);
+                    });
                 })
                 // .then(item => console.log(item))
                 .catch(error => console.log(error.message));
-}
+    }
 
-getWearher(urlKyiv, outputKyiv);
-getWearher(urlLondon, outputLondon);
-getWearher(urlNewYork, outputNewYork);
+setInterval(getWearher(urlKyiv, outputKyiv), 600000);
+setInterval(getWearher(urlLondon, outputLondon), 600000);
+setInterval(getWearher(urlNewYork, outputNewYork), 600000);
