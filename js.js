9bb8009602742f1237ced9e98509612bcb434451
Saoa35 <saoamah@gmail.com>
2022-08-07 19:22:57 +0200

    changed backgroundImg

--- js.js
+++ js.js
@@ -3,12 +3,12 @@ let urlKyiv = 'http://api.openweathermap.org/data/2.5/weather?id=703448&appid=bf
 let urlLondon = 'http://api.openweathermap.org/data/2.5/weather?id=2643743&appid=bf35cac91880cb98375230fb443a116f';
 let urlNewYork = 'http://api.openweathermap.org/data/2.5/weather?id=5128638&appid=bf35cac91880cb98375230fb443a116f';
 
-let summergImg = 'url("Summer.jpg") no-repeat';
+let backgroundImg = 'url("backgroundImg.jpg") no-repeat';
     
 let body = document.querySelector('body');
 
 
-body.style.background = summergImg;
+body.style.background = backgroundImg;
 
 let footer = document.querySelector('footer');
 
@@ -19,6 +19,7 @@ let footer = document.querySelector('footer');
                     const cityName = json.name;
                     const weatherDescr = json.weather[0].description;
                     let temp = Math.round(json.main.temp - 273.15) + ' ℃';
+
                     const img = document.createElement('img');
                     img.src = 'http://openweathermap.org/img/wn/' + json.weather[0]['icon'] + '@2x.png';
                     let ul = document.createElement('ul');
@@ -31,35 +32,49 @@ let footer = document.querySelector('footer');
 
                     div.append(ul);
 
-                    let lii = document.querySelector('section div ul :nth-child(2)');
+                    let currentTemp = document.querySelector('section div ul :nth-child(2)').textContent.includes('℃');
+                    // let currentTemp2 = document.querySelector('section div ul :nth-child(2)').textContent.includes('°F');
 
-                    let text = lii.textContent;
+                    // let allLi = document.querySelector('section div ul :nth-child(2)').textContent;
+                    // let arr = Array.from(allLi);
 
-                    let inc = text.includes('℃');
-
-                    console.log(inc);
+                    // console.log(arr);
                     
 
                     footer.addEventListener('click', function() {
 
-                        
+                        if(currentTemp) {
+                            div.textContent = '';
+                            temp = Math.trunc((parseInt(temp) * (9/5)) + 32) + ' °F';
+                            let ul = document.createElement('ul');
 
+                            [cityName, temp, weatherDescr, img].map(el => {
+                                let li = document.createElement('li');
+                                li.append(el);
+                                ul.append(li);
+                            });
 
-                    // if(temp) {
-                        div.textContent = '';
-                        temp = Math.trunc((parseInt(temp) * (9/5)) + 32) + ' °F';
-                        let ul = document.createElement('ul');
+                                div.append(ul);
 
-                        [cityName, temp, weatherDescr, img].map(el => {
-                            let li = document.createElement('li');
-                            li.append(el);
-                            ul.append(li);
-                        });
+                            footer.textContent = 'Change to Celsius';
 
-                            div.append(ul);
+                            // console.log(document.querySelector('section div ul :nth-child(2)').textContent);
 
-                        footer.textContent = 'Change to Celsius';
-                        // } else temp;
+                        } else  window.location.reload();  
+                            // if(currentTemp2) {
+                                // div.textContent = '';
+                                // let ul = document.createElement('ul');
+                                // temp = Math.round(json.main.temp - 273.15) + ' ℃';
+                                // [cityName, temp, weatherDescr, img].map(el => {
+                                //     let li = document.createElement('li');
+                                //     li.append(el);
+                                //     ul.append(li);
+                                // });
+            
+                                // div.append(ul);
+                                // footer.textContent = 'Change to Fahrenheit';
+                            // }
+
                     });
 
                 })
